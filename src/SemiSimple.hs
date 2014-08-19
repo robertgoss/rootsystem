ee09cfa6ea5a1df0f31cbce3b54ef0a57acdca23
Robert <goss.robert@gmail.com>
2014-08-19 12:45:32 +0000

    Generate root system for simple algebra's.

--- src/SemiSimple.hs
+++ src/SemiSimple.hs
@@ -23,6 +23,9 @@ data SemiSimple = SemiSimple Int [Simple] deriving(Eq,Ord,Show)
 fromSimples :: Int -> [Simple] -> SemiSimple
 fromSimples torus simples = SemiSimple torus (sort simples)
 
+product :: SemiSimple -> SemiSimple -> SemiSimple
+product (SemiSimple torus1 simples1) (SemiSimple torus2 simples2) = fromSimples (torus1+torus2) (simples1++simples2)
+
 dimSimple :: Simple -> Int
 dimSimple (A n) = n*n + 2*n
 dimSimple (B n) = n * (2*n + 1)
@@ -52,7 +55,31 @@ rank :: SemiSimple -> Int
 rank (SemiSimple torus simples) = torus + sum (map rankSimple simples)
 
 
-
+rootSystemSimple :: Simple -> BasicRootSystem
+rootSystemSimple (A n) = fromRoots $ map (BasicRoot . swapRoot (n+1)) [1..n]
+    where swapRoot m i = M.setElem 1 (1,i) $ M.setElem 1 (-1,i+1) $ M.zero 1 m
+rootSystemSimple (B n) = fromRoots $ negRoot : aRoots
+    where (BasicRootSystem _ aRoots) = rootSystemSimple (A (n-1))
+          negRoot = BasicRoot $ M.setElem 1 (1,1) $ M.zero 1 n
+rootSystemSimple (C n) = fromRoots $ negLongRoot : aRoots
+    where (BasicRootSystem _ aRoots) = rootSystemSimple (A (n-1))
+          negLongRoot = BasicRoot $ M.setElem 2 (1,1) $ M.zero 1 n
+rootSystemSimple (D n) = fromRoots $ negSwapRoot : aRoots
+    where (BasicRootSystem _ aRoots) = rootSystemSimple (A (n-1))
+          negSwapRoot = BasicRoot $ M.setElem 1 (1,1) $ M.setElem 1 (1,2) $ M.zero 1 n
+rootSystemSimple G2 = fromRoots $ map (BasicRoot . M.fromList 1 3) $ [[0,1,-1] , [1,-2,1]]
+rootSystemSimple F4 = fromRoots $ map (BasicRoot . M.fromList 1 4) $ [[0,1,-1,0],
+                                                                      [0,0,1,-1],
+                                                                      [0,0,0,1],
+                                                                      [1/2,-1/2,-1/2,-1/2]]
+rootSystemSimple E8 = fromRoots $ excepRoot : spin14Roots'
+    where (BasicRootSystem _ spin14Roots) = rootSystemSimple (D 7)
+          spin14Roots' = map (BasicRoot . M.extendTo 1 8 . coroot) spin14Roots
+          excepRoot = BasicRoot $ M.fromList 1 8 $ replicate 8 (-1/2)
+rootSystem E7 = fromRoots $ take 7 e8roots
+    where (BasicRootSystem _ e8roots) = rootSystemSimple E8
+rootSystem E6 = fromRoots $ take 6 e8roots
+    where (BasicRootSystem _ e8roots) = rootSystemSimple E8
 
 instance Arbitrary Simple where
     arbitrary = do n <- arbitrary
