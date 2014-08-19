b30eade2341dd1f15b99dc99859531125a9a8827
Robert <goss.robert@gmail.com>
2014-08-19 12:16:57 +0000

    Added rank and dimension functions for a semisimple algebra

--- src/SemiSimple.hs
+++ src/SemiSimple.hs
@@ -20,16 +20,33 @@ data Simple = A Int
 
 data SemiSimple = SemiSimple Int [Simple]
 
+dimSimple :: Simple -> Int
+dimSimple (A n) = n*n + 2*n
+dimSimple (B n) = n * (2*n + 1)
+dimSimple (C n) = n * (2*n + 1)
+dimSimple (D n) = n * (2*n - 1)
+dimSimple G2 = 14
+dimSimple F4 = 52
+dimSimple E6 = 78
+dimSimple E7 = 133
+dimSimple E8 = 248
+
+dim :: SemiSimple -> Int
+dim (SemiSimple torus simples) = torus + sum (map dimSimple simples)
+
 rankSimple :: Simple -> Int
-rankSimple (A n) = n*n + 2*n
-rankSimple (B n) = n * (2*n + 1)
-rankSimple (C n) = n * (2*n + 1)
-rankSimple (D n) = n * (2*n - 1)
-rankSimple G2 = 14
-rankSimple F4 = 52
-rankSimple E6 = 78
-rankSimple E7 = 133
-rankSimple E8 = 248
+rankSimple (A n) = n
+rankSimple (B n) = n
+rankSimple (C n) = n
+rankSimple (D n) = n
+rankSimple G2 = 2
+rankSimple F4 = 4
+rankSimple E6 = 6
+rankSimple E7 = 7
+rankSimple E8 = 8
+
+rank :: SemiSimple -> Int
+rank (SemiSimple torus simples) = torus + sum (map rankSimple simples)
 
 
 
