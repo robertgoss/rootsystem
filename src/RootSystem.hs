766540a0da905a65f3264f8665ab32a51fa61ad4
Robert <goss.robert@gmail.com>
2014-08-21 09:10:14 +0000

    Added basic classed and type for sub systems and double sub systems

--- src/RootSystem.hs
+++ src/RootSystem.hs
@@ -27,6 +27,20 @@ class RootSystem r where
     rank :: r -> Int
     cartanAlgebra :: r -> CartanAlgebra
 
+class SubSystem s where
+    type SystemType
+    superSystem :: (RootSystem SystemType) => s -> SystemType
+    generatorImages :: (Root RootType) => s -> [RootType]
+    subSystem :: (RootSystem SystemType) => s -> SystemType
+
+class SubSystem2 s where
+    type SystemType2
+    dualSuperSystem :: (RootSystem SystemType) => s -> SystemType
+    generatorImages1 :: (Root RootType) => s -> [RootType]
+    subSystem1 :: (RootSystem SystemType) => s -> SystemType
+    generatorImages2 :: (Root RootType) => s -> [RootType]
+    subSystem2 :: (RootSystem SystemType) => s -> SystemType
+
 rootsScan :: (RootSystem r) => r -> [([RootType],[RootType])]
 rootsScan = generateScan (flip reflect) . generators
 
@@ -46,6 +60,8 @@ dim system = rank system + length (roots system)
 
 newtype BasicRoot = BasicRoot (Vector QQ) deriving (Eq,Show)
 data BasicRootSystem = BasicRootSystem CartanAlgebra [BasicRoot]
+data BasicSubSystem = BasicSubSystem BasicRootSystem BasicRootSystem [BasicRoot]
+data BasicSubSystem2 = BasicSubSystem2 BasicRootSystem BasicRootSystem BasicRootSystem [BasicRoot] [BasicRoot]
 
 instance Ord BasicRoot where
     (BasicRoot v1) `compare` (BasicRoot v2) = (getRow 1 v1) `compare` (getRow 1 v2)
@@ -100,6 +116,31 @@ canonicalRootSystem rootsystem = BasicRootSystem cartan roots
     where   gens = generators rootsystem :: [RootType]
             roots = map (BasicRoot . coroot) gens
             cartan = cartanAlgebra rootsystem
+
+
+instance SubSystem BasicSubSystem where
+    type SystemType = BasicRootSystem
+    superSystem (BasicSubSystem super _ _) = super
+    generatorImages (BasicSubSystem _ _ gens) = gens
+    subSystem (BasicSubSystem _ sub _) = sub
+
+fullSubSystem :: BasicRootSystem -> [BasicRoot] -> BasicSubSystem
+fullSubSystem super gens = BasicSubSystem super sub gens
+    where sub = fromRoots gens
+
+instance SubSystem2 BasicSubSystem2 where
+    type SystemType2 = BasicRootSystem
+    dualSuperSystem (BasicSubSystem2 super _ _ _ _) = super
+    generatorImages1 (BasicSubSystem2 _ _ _ _ gens2) = gens2
+    subSystem1 (BasicSubSystem2 _ sub1 _ _ _) = sub1
+    generatorImages2(BasicSubSystem2 _ _ _ gens1 _) = gens1
+    subSystem2 (BasicSubSystem2 _ _ sub2 _ _) = sub2
+
+fullSubSystem2 :: BasicRootSystem -> [BasicRoot] -> [BasicRoot] -> BasicSubSystem2
+fullSubSystem2 super gens1 gens2 = BasicSubSystem2 super sub1 sub2 gens1 gens2
+    where sub1 = fromRoots gens1
+          sub2 = fromRoots gens2
+
 
 instance Arbitrary BasicRoot where
     arbitrary = do a <- arbitrary
