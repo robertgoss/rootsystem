99a16229e9727a1b217c06df437e0fc83224b7a9
Robert <goss.robert@gmail.com>
2014-09-03 00:28:39 +0000

    Make sure that we compare coroots from spin after pading.

--- src/Test/TestSpin.hs
+++ src/Test/TestSpin.hs
@@ -5,6 +5,8 @@ import Test.Tasty.SmallCheck as SC
 import Test.Tasty.QuickCheck as QC
 import Test.Tasty.HUnit
 
+import Data.Matrix
+
 import RootSystem
 import Spin
 
@@ -15,5 +17,11 @@ testSpinUnit = testGroup "Unit tests" []
 
 bCoroot = BasicRoot . coroot
 
+padEqual v w = v' == w'
+    where v' | ncols v >= ncols w = v
+             | otherwise = v <|> zero 1 (ncols w - ncols v)
+          w' | ncols w >= ncols v = w
+             | otherwise = w <|> zero 1 (ncols v - ncols w)
+
 testSpinReflectCoroot = QC.testProperty "Coroot should push forward reflect to the reflect of basic roots" $ \(r1,r2) ->
-    coroot ((r1::SpinRoot) `reflect` r2) == coroot ((bCoroot r1) `reflect` (bCoroot r2))
+    coroot ((r1::SpinRoot) `reflect` r2) `padEqual` coroot ((bCoroot r1) `reflect` (bCoroot r2))
