08f0325efc311700237f8719175fd6742c5e5746
Robert <goss.robert@gmail.com>
2014-09-06 11:25:41 +0000

    Slight genralisation not longer need branch on part of twist.

--- src/E8.hs
+++ src/E8.hs
@@ -184,10 +184,11 @@ sign2Mult_2 sign2 sign4 sign perm = makeType3 False sign4' $ SpinElement (mid `c
           perm' = (perm `Perm.combine` sign2swap) `Perm.combine` midSwap
 
 sign2Mult_4 sign2 sign4 sign perm | signType twsign2 == 0 = makeType3 True sign4 $ SpinElement sign perm
-                                  | at 1 sign2 == -1 = sign2Mult tw $ (sign2Mult_2 twsign2 sign4 sign perm)
-                                  | otherwise = undefined
+                                  | signType twsign2 == 2 = sign2Mult tw $ (sign2Mult_2 twsign2 sign4 sign perm)
+                                  | signType twsign2 == 4 = undefined
     where tw = twist sign4
           twsign2 = tw `combine` sign2
+          spin4_2 = combine
 
 instance WeylGroupElement E8WeylElement E8Root where
 
