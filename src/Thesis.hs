3032a79d3055c05be3c59bc7931826b9e79c4b85
Robert Goss <goss.robert@gmail.com>
2014-09-30 17:42:53 +0100

    Add double quotient structures and tests in S3E7

--- src/Thesis.hs
+++ src/Thesis.hs
@@ -34,6 +34,10 @@ spin16OneS3E7 = generatePointed multiply quoS3E7Gens one'
 	where quoS3E7Gens = map (image E8Spin16Quotient) $ Weyl.generators s3e7Group
 	      one' = one spin16QuotientGroup
 
+spin4spin12OneS3S1E6 = generatePointed multiply quoS3S1E6Gens one'
+	where quoS3S1E6Gens = map (image S3E7Spin4Spin12Quotient) $ Weyl.generators s3s1e6Group
+	      one' = one spin4spin12QuotientGroup
+
 s :: E8WeylElement
 s = simpleReflection $ E8SRoot $ Signs.identity 8
 
@@ -46,6 +50,16 @@ x8s = x8 `multiply` s
 spin16X8SS3E7 = generatePointed multiply quoS3E7Gens x8s'
 	where quoS3E7Gens = map (image E8Spin16Quotient) $ Weyl.generators s3e7Group
 	      x8s' = image E8Spin16Quotient x8s
+
+x7 = swap56 `multiply` signSwap56
+	where swap56 = simpleReflection . E8SpinRoot $ SwapRoot 5 6
+	      signSwap56 = simpleReflection . E8SpinRoot $ SignSwapRoot 5 6
+
+x7s = x7 `multiply` s 
+
+spin4spin12X7SS3S1E6 = generatePointed multiply quoS3S1E6Gens x7s'
+	where quoS3S1E6Gens = map (image S3E7Spin4Spin12Quotient) $ Weyl.generators s3s1e6Group
+	      x7s' = image S3E7Spin4Spin12Quotient x7s
 
 commute a b = a `multiply` b == b `multiply` a
 
