f7ed01a7ff52b35cd2004ddf313a7e7252c66bdc
Robert <goss.robert@gmail.com>
2014-10-02 03:33:07 -0700

    Apply constructor and nRoots in number not roots.

--- src/WeightLattice.hs
+++ src/WeightLattice.hs
@@ -22,7 +22,7 @@ instance Ord PrincipleWeight where
               ys' = ys ++ replicate (m-n) 0
 
 add :: PrincipleWeight -> PrincipleWeight -> PrincipleWeight
-add (PWeight xs) (PWeight ys) = zipWith (+) xs' ys'
+add (PWeight xs) (PWeight ys) = PWeight $ zipWith (+) xs' ys'
   where m = length xs
         n = length ys
         xs' = xs ++ replicate (n-m) 0
@@ -35,7 +35,7 @@ weightLattice = WL
 
 generators :: (RootSystem r rt) => WeightLattice r -> [PrincipleWeight]
 generators (WL r) = map basicWeight [1..nRoots] 
-    where nRoots = simpleRoots r
+    where nRoots = length $ simpleRoots r
           basicWeight i = PWeight $ replicate (i-1) 0 ++ [1] 
 
 weylDimension :: (RootSystem r rt) => WeightLattice r -> PrincipleWeight -> Int
