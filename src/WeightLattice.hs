f665d99d064ce4a7e7a584a8c6f070ce55458953
Robert <goss.robert@gmail.com>
2014-10-03 03:41:19 -0700

    Added constructor for basic lattice

--- src/WeightLattice.hs
+++ src/WeightLattice.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 module WeightLattice where
@@ -5,6 +6,7 @@ module WeightLattice where
 import RootSystem
 import Generate
 
+import Data.Set as Set
 import Data.Map as Map
 
 newtype PrincipleWeight = PWeight [Int] deriving(Show)
@@ -32,10 +34,38 @@ add (PWeight xs) (PWeight ys) = PWeight $ zipWith (+) xs' ys'
         xs' = xs ++ replicate (n-m) 0
         ys' = ys ++ replicate (m-n) 0
 
+negate (PWeight xs) = PWeight $ Prelude.map (0-) xs
+
+basicWeight i = PWeight $ replicate (i-1) 0 ++ [1]
+
+weightProduct :: PrincipleWeight -> PrincipleWeight -> Integer
+weightProduct (PWeight xs) (PWeight ys)= fromIntegral . sum $ zipWith (*) xs ys
+
 class (RootSystem r rt) => WeightLattice wl r rt | wl -> r, wl -> rt where
     generators :: wl -> [PrincipleWeight]
     innerProduct :: wl -> PrincipleWeight -> rt -> Integer
-    assosiatedWeight :: wl -> rt -> PrincipleWeight
+    associatedWeight :: wl -> rt -> PrincipleWeight
     underlyingSystem :: wl -> r
 
-  
+instance (RootSystem r rt) => WeightLattice (BasicLattice r rt) r rt where
+    generators (BasicLattice r _) = Prelude.map basicWeight [1..nRoots]
+        where nRoots = length $ simpleRoots r
+    underlyingSystem (BasicLattice r _) = r
+    associatedWeight (BasicLattice _ rMap) root = rMap ! root
+    innerProduct (BasicLattice _ rMap) weight root = weightProduct weight $ rMap ! root
+
+basicLattice :: (RootSystem r rt) => r -> BasicLattice r rt
+basicLattice r = BasicLattice r rMap
+    where rMap = Map.fromAscList pairs
+          pairs = generateWithFailure comb $ zip (rGens++rGensN) (pGens++pGensN)
+          rootSet = Set.fromList $ roots r
+          rGens = simpleRoots r
+          rGensN = Prelude.map RootSystem.negate rGens
+          pGens = Prelude.map basicWeight [1..(length rGens)]
+          pGensN = Prelude.map WeightLattice.negate pGens
+          comb (r1,pw1) (r2,pw2) = case r1 `RootSystem.add` r2 of
+                                      Nothing -> Nothing
+                                      (Just rSum) -> if rSum `Set.member` rootSet then
+                                                        Just (rSum, WeightLattice.add pw1 pw2)
+                                                     else
+                                                        Nothing
