3ecdb6073925665a5623e37f8a1d9ae683dbb6c5
Robert Goss <goss.robert@gmail.com>
2014-10-05 01:50:08 +0100

    Use negation property to get associated weight for non positive roots

--- src/WeightLattice.hs
+++ src/WeightLattice.hs
@@ -7,8 +7,8 @@ import RootSystem
 import Generate
 import qualified Data.Set as Set
 
-import Data.Set as Set
-import Data.Map as Map
+import qualified Data.Set as Set
+import qualified Data.Map as Map
 
 newtype PrincipleWeight = PWeight [Int] deriving(Show)
 data BasicLattice r rt = BasicLattice r (Map.Map rt PrincipleWeight)
@@ -32,7 +32,7 @@ zeroWeight :: PrincipleWeight
 zeroWeight = PWeight []
 
 negate :: PrincipleWeight -> PrincipleWeight
-negate (PWeight xs) = PWeight $ Prelude.map (0-) xs
+negate (PWeight xs) = PWeight $ map (0-) xs
 
 add :: PrincipleWeight -> PrincipleWeight -> PrincipleWeight
 add (PWeight xs) (PWeight ys) = PWeight $ zipWith (+) xs' ys'
@@ -42,7 +42,7 @@ add (PWeight xs) (PWeight ys) = PWeight $ zipWith (+) xs' ys'
         ys' = ys ++ replicate (m-n) 0
 
 scaleWeight :: Int -> PrincipleWeight -> PrincipleWeight
-scaleWeight n (PWeight xs)  = PWeight $ Prelude.map (*n) xs
+scaleWeight n (PWeight xs)  = PWeight $ map (*n) xs
 
 basicWeight i = PWeight $ replicate (i-1) 0 ++ [1]
 
@@ -59,10 +59,11 @@ class (RootSystem r rt) => WeightLattice wl r rt | wl -> r, wl -> rt where
 
 
 instance (RootSystem r rt) => WeightLattice (BasicLattice r rt) r rt where
-    generators (BasicLattice r _) = Prelude.map basicWeight [1..nRoots]
+    generators (BasicLattice r _) = map basicWeight [1..nRoots]
         where nRoots = length $ simpleRoots r
     underlyingSystem (BasicLattice r _) = r
-    associatedWeight (BasicLattice _ rMap) root = rMap ! root
+    associatedWeight (BasicLattice _ rMap) root | positive root = (Map.!) rMap root
+                                                | otherwise = (WeightLattice.negate) $ (Map.!) rMap (RootSystem.negate root)
     innerProduct lattice@(BasicLattice _ rMap) weight root = weightProduct weight rWeight
       where rWeight = associatedWeight lattice root
 
@@ -72,7 +73,7 @@ basicLattice r = BasicLattice r rMap
           pairs = generateWithFailure comb $ zip rGens pGens
           rootSet = Set.fromList $ roots r
           rGens = simpleRoots r
-          pGens = Prelude.map basicWeight [1..(length rGens)]
+          pGens = map basicWeight [1..(length rGens)]
           comb (r1,pw1) (r2,pw2) = case r1 `RootSystem.add` r2 of
                                       Nothing -> Nothing
                                       (Just rSum) -> if rSum `Set.member` rootSet then
@@ -85,9 +86,11 @@ weylDimension lattice weight = product numerator `div` product denominator
   where system = underlyingSystem lattice
         pRoots = positiveRoots system
         sRoots = simpleRoots system
-        sLengths = Prelude.map (fromInteger . lengthSq) sRoots
-        rho = Prelude.foldl WeightLattice.add zeroWeight $ Prelude.map (associatedWeight lattice) sRoots
+        sLengths = map (fromInteger . lengthSq) sRoots
+        rho = foldl WeightLattice.add zeroWeight $ map (associatedWeight lattice) sRoots
         weightSum = coordScale sLengths $ weight `WeightLattice.add` rho
         rho' = coordScale sLengths rho
-        numerator = Prelude.map (innerProduct lattice weightSum) pRoots
-        denominator = Prelude.map (innerProduct lattice rho') pRoots
+        numerator = map (innerProduct lattice weightSum) pRoots
+        denominator = map (innerProduct lattice rho') pRoots
+
+
